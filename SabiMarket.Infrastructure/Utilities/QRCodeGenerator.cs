c3cd714678a90aa980b7ebb92a33bc2842c8965e
Ngwueche Wisdom <wisdomngwueche@gmail.com>
2024-12-23 07:19:35 +0100

    added QRCode Generator

--- SabiMarket.Infrastructure/Utilities/QRCodeGenerator.cs
+++ SabiMarket.Infrastructure/Utilities/QRCodeGenerator.cs
@@ -10,28 +10,25 @@ using QRCoder;
 
 namespace SabiMarket.Infrastructure.Utilities
 {
-    public class QRCodeGeneratorService
+    public static class QRCodeGeneratorService
     {
-        public byte[] GenerateQRCode<T>(T data)
+        public static byte[] GenerateQRCode<T>(T data)
         {
-
+            byte[] qrCodeImage;
             // Serialize the data into JSON format
             var qrData = JsonSerializer.Serialize(data, new JsonSerializerOptions
             {
                 WriteIndented = true // Makes the JSON readable
             });
 
-            // Generate QR Code
-            using var qrGenerator = new QRCodeGenerator();
-            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrData, QRCodeGenerator.ECCLevel.Q);
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrData, QRCodeGenerator.ECCLevel.Q))
+            using (PngByteQRCode qrCode = new PngByteQRCode(qrCodeData))
+            {
+                qrCodeImage = qrCode.GetGraphic(20);
+            }
 
-            //using var qrCode = new QRCode(qrCodeData);
-            //using Bitmap qrCodeImage = qrCode.GetGraphic(20); // 20 is the pixel size
-
-            // Convert QR Code to byte array
-            using var ms = new MemoryStream();
-            // qrCodeImage.Save(ms, ImageFormat.Png);
-            return ms.ToArray();
+            return qrCodeImage;
         }
     }
 }
