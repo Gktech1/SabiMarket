3e145054ecd00bc7190dcdc81f4f678f472f50aa
Ngwueche Wisdom <wisdomngwueche@gmail.com>
2024-12-28 07:45:19 +0100

    .

---
+++ SabiMarket.Infrastructure/Repositories/LevyRespository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SabiMarket.Domain.Entities.LevyManagement;
+
+namespace SabiMarket.Infrastructure.Repositories
+{
+    public class LevyRespository
+    {
+        public void AddLevy(LevyPayment entity)
+        {
+
+        }
+    }
+}
