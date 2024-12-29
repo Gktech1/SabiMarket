56a6349aba31cd611a4e22961deec31c938334f5
Ngwueche Wisdom <wisdomngwueche@gmail.com>
2024-12-29 19:24:11 +0100

    abstracted services to ServiceRegistry class

--- SabiMarket.API/Program.cs
+++ SabiMarket.API/Program.cs
@@ -1,9 +1,9 @@
-
 using FluentValidation;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SabiMarket.API.Extensions;
 using SabiMarket.API.Middlewares;
+using SabiMarket.API.ServiceExtensions;
 using SabiMarket.Application.Interfaces;
 using SabiMarket.Application.Validators;
 using SabiMarket.Domain.Entities.UserManagement;
@@ -17,49 +17,8 @@ namespace SabiMarket.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
+            builder.Services.AddInfrastructure(builder.Configuration);
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
-            // Add DbContext
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            // Add Identity
-            builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-            // Add FluentValidation
-            builder.Services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
-
-            // Configure Identity options (optional)
-            builder.Services.Configure<IdentityOptions>(options =>
-            {
-                // Password settings
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireUppercase = true;
-                options.Password.RequireNonAlphanumeric = true;
-                options.Password.RequiredLength = 8;
-
-                // Lockout settings
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-                options.Lockout.MaxFailedAccessAttempts = 5;
-                options.Lockout.AllowedForNewUsers = true;
-
-                // User settings
-                options.User.RequireUniqueEmail = true;
-            });
-
-            // Add custom error handling
-            // builder.Services.AddCustomErrorHandling(); // Add this BEFORE var app = builder.Build()
-
-
-            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-            builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
---
+++ SabiMarket.API/ServiceExtensions/ServiceRegistry.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SabiMarket.API.Middlewares;
+using SabiMarket.Application.Validators;
+using SabiMarket.Domain.Entities.UserManagement;
+using SabiMarket.Infrastructure.Data;
+
+namespace SabiMarket.API.ServiceExtensions
+{
+    public static class ServiceRegistry
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // Add Identity
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add FluentValidation
+            services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
+
+            // Configure Identity options (optional)
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 8;
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
+            // Add custom error handling
+            // builder.Services.AddCustomErrorHandling(); // Add this BEFORE var app = builder.Build()
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<RequestTimeLoggingMiddleware>();
+
+
+        }
+    }
+}
