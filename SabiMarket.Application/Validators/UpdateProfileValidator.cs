2ccbfe39db92aac1c5e203a284189c6255d25ae1
Olukunle Edun <favourblessing1@gmail.com>
2025-01-09 08:32:45 +0100

    feature update on update profile

--- SabiMarket.Application/Validators/UpdateProfileValidator.cs
+++ SabiMarket.Application/Validators/UpdateProfileValidator.cs
@@ -2,15 +2,9 @@
 using Microsoft.AspNetCore.Identity;
 using SabiMarket.Application.DTOs.Requests;
 using SabiMarket.Domain.Entities.UserManagement;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SabiMarket.Application.Validators
 {
-    // UpdateProfileValidator.cs
     public class UpdateProfileValidator : AbstractValidator<UpdateProfileDto>
     {
         private readonly UserManager<ApplicationUser> _userManager;
@@ -30,14 +24,13 @@ namespace SabiMarket.Application.Validators
             RuleFor(x => x.Email)
                 .NotEmpty().WithMessage("Email is required")
                 .EmailAddress().WithMessage("Please provide a valid email address")
-                .MustAsync(BeUniqueEmail).WithMessage("This email is already in use")
-                .When((dto, email) => !string.IsNullOrEmpty(email));
-        }
-
-        private async Task<bool> BeUniqueEmail(string email, CancellationToken cancellationToken)
-        {
-            var user = await _userManager.FindByEmailAsync(email);
-            return user == null;
+                .MustAsync(async (model, email, cancellation) =>
+                {
+                    if (string.IsNullOrEmpty(email)) return false;
+                    var user = await _userManager.FindByEmailAsync(email);
+                    return user == null;
+                })
+                .WithMessage("This email is already in use");
         }
     }
 }
