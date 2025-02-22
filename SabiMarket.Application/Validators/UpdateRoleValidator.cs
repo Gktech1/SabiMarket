d97f14639cfa9e94e71a60340c4b321664b20c20
Olukunle Edun <favourblessing1@gmail.com>
2025-02-22 08:45:54 +0100

    features updates roles

--- SabiMarket.Application/Validators/UpdateRoleValidator.cs
+++ SabiMarket.Application/Validators/UpdateRoleValidator.cs
@@ -1,27 +1,19 @@
 ﻿using FluentValidation;
 using SabiMarket.Application.DTOs.Requests;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SabiMarket.Application.Validators
+public class UpdateRoleValidator : AbstractValidator<UpdateRoleRequestDto>
 {
-    // Validators/UpdateRoleValidator.cs
-    public class UpdateRoleValidator : AbstractValidator<UpdateRoleRequestDto>
+    public UpdateRoleValidator()
     {
-        public UpdateRoleValidator()
-        {
-            RuleFor(x => x.Name)
-                .NotEmpty().WithMessage("Role name is required")
-                .MaximumLength(50).WithMessage("Role name cannot exceed 50 characters")
-                .Matches("^[a-zA-Z0-9\\s-]+$").WithMessage("Role name can only contain letters, numbers, spaces and hyphens");
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Role name is required")
+            .MaximumLength(50).WithMessage("Role name cannot exceed 50 characters")
+            .Matches("^[a-zA-Z0-9\\s-_]+$").WithMessage("Role name can only contain letters, numbers, spaces, hyphens and underscores");
 
-            RuleFor(x => x.Permissions)
-                .NotNull().WithMessage("Permissions cannot be null")
-                .Must(permissions => permissions.All(p => RolePermissionConstants.AllPermissions.Contains(p)))
-                .WithMessage($"Permissions must be from the allowed list: {string.Join(", ", RolePermissionConstants.AllPermissions)}");
-        }
+        RuleFor(x => x.Permissions)
+            .NotNull().WithMessage("Permissions cannot be null")
+            .NotEmpty().WithMessage("At least one permission is required")
+            .Must(permissions => permissions != null && permissions.All(p => RolePermissionConstants.AllPermissions.Contains(p)))
+            .WithMessage($"Permissions must be from the allowed list: {string.Join(", ", RolePermissionConstants.AllPermissions)}");
     }
 }
