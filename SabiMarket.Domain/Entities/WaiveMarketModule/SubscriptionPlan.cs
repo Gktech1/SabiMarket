544caa5c95361eac7dde9ec3fda53036dd648bf1
Ngwueche Wisdom <wisdomngwueche@gmail.com>
2025-04-21 08:55:30 +0100

    migration issues to fix

--- SabiMarket.Domain/Entities/WaiveMarketModule/SubscriptionPlan.cs
+++ SabiMarket.Domain/Entities/WaiveMarketModule/SubscriptionPlan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace SabiMarket.Domain.Entities.WaiveMarketModule
     public class SubscriptionPlan : BaseEntity
     {
         public string Frequency { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Amount { get; set; }
         public ICollection<Subscription> Subscriptions { get; set; }
     }
