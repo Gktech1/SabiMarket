26cb4c4ff7c6b4285efb897a4f07cd80ccac7e41
Olukunle Edun <favourblessing1@gmail.com>
2025-04-21 14:14:08 +0100

    updates on Db fixed

--- SabiMarket.Infrastructure/Migrations/20250406122357_OfficeMarketAssitofficer.cs
+++ SabiMarket.Infrastructure/Migrations/20250406122357_OfficeMarketAssitofficer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -57,3 +57,4 @@ namespace SabiMarket.Infrastructure.Migrations
         }
     }
 }
+*/
