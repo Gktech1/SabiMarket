dc0d189750fee9a1a0b62a3dde6917ff166a3254
Olukunle Edun <favourblessing1@gmail.com>
2025-05-12 10:54:49 +0100

    update trader login

--- SabiMarket.Infrastructure/Services/AuthenticationService.cs
+++ SabiMarket.Infrastructure/Services/AuthenticationService.cs
@@ -454,7 +454,7 @@ namespace SabiMarket.Infrastructure.Services
                     {
                         details.Add("traderId", user.Trader.Id);
                         details.Add("traderbusinessName", user.Trader.BusinessName);
-                        details.Add("traderbusinessName", user.Trader.TIN);
+                        details.Add("traderbusinessType", user.Trader.TIN);
                         details.Add("businessType", user.Trader.BusinessType);
                         details.Add("traderMarketId", user.Trader.MarketId);
                         details.Add("traderCaretakerId", user.Trader.CaretakerId);
