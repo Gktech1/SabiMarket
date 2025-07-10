97cb7fae9d0a0873cc0bcc0fd93316fd75075f68
Ngwueche Wisdom <wisdomngwueche@gmail.com>
2025-07-10 08:30:30 +0100

    ran migration

---
+++ SabiMarket.Infrastructure/Migrations/20250710072948_isresolvedflag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SabiMarket.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class isresolvedflag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsResolved",
+                table: "CustomerFeedbacks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsResolved",
+                table: "CustomerFeedbacks");
+        }
+    }
+}
